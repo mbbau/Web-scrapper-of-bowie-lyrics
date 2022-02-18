286eec9e3dd9b20edccef16cc4b4d07ed16c8d5f
Matías Baudino <61053776+mbbau@users.noreply.github.com>
2022-02-18 07:59:48 -0300

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -31,7 +31,7 @@ soup = BeautifulSoup(lyrics_page, "html.parser")
 # with this print, I saw the main structure of the html and found out that I need to 
 # look for the tag named a with the href and the text within it. In there I will find
 # the name of the song and a link to its lyric.
-#print(soup.prettify())
+# print(soup.prettify())
 # list_of_tags = list()
 # for tag in soup.find_all(True):
 #     list_of_tags.append(tag.name)
@@ -47,4 +47,39 @@ for tag in tags:
     cur.execute('INSERT OR IGNORE INTO Tracks (track, url) VALUES ( ?, ? )', ( song, link, ) )
 conn.commit()
 
+# I will inspect where the lyrics are in every link by analyzing the html file
+# url2 = "https://www.lyrics.com/lyric/35488143/David+Bowie/Ashes+to+Ashes"
+# song_page = urllib.request.urlopen(url2, context=ctx)
+# second_soup = BeautifulSoup(song_page, 'html.parser')
+# print(second_soup.prettify())
+# list_of_tags_two = list()
+# for tag in second_soup.find_all(True):
+#       list_of_tags_two.append(tag.name)
+# print(second_soup.pre.text)
 
+cur.execute('SELECT id, url FROM Tracks')
+counts = dict()
+for links in cur:
+    new_url = "https://www.lyrics.com" + links[1]
+    song_page = urllib.request.urlopen(new_url, context=ctx)
+    second_soup = BeautifulSoup(song_page, 'html.parser')    
+    try:
+        song_lyric = second_soup.pre.text
+        song_lyric = song_lyric.strip()
+        song_lyric = song_lyric.lower()
+        words = song_lyric.split()
+        for word in words:
+            if len(word) < 4 : continue
+            counts[word] = counts.get(word,0) + 1
+    except:
+        continue
+
+x = sorted(counts, key=counts.get, reverse=True)
+highest = None
+lowest = None
+for k in x[:100]:
+    if highest is None or highest < counts[k] :
+        highest = counts[k]
+    if lowest is None or lowest > counts[k] :
+        lowest = counts[k]
+print('Range of counts:',highest,lowest)
